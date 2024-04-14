b8c4c38c5b59a4b799aee90cb06eaf7ff5a511f1
Default Box <134133080+thedefaultbox@users.noreply.github.com>
2024-04-14 19:40:05 +1000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,12 +3,12 @@ const app = express();
 
 app.get("/", (req, res) => {
     if (req.headers["user-agent"].includes("Twitterbot")) {
-        res.setHeader("Location", "https://x.ai");
-
+        res.setHeader("Location", "https://minecraft.net/");
+        res.setHeader('Title', "Changes to Minecraft's Online Multiplayer System");
         return;
     }
 
-    res.redirect(301, "https://chat.openai.com");
+    res.redirect(301, "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 });
 
 const port = process.env.PORT || 3000;
